995e4ede2f605d69e4f49092513e78fa89e3298a
Cesar Bassani <cesarbassani@Cesars-MacBook-Pro.local>
2019-06-20 16:33:19 -0400

    Criacao API do Sistema de Acompanhamento e Gerenciamento de abates PecBR/AcaBoi

---
+++ src/main/java/com/pecbr/abateapi/AbateApiApplication.java
@@ -0,0 +1,13 @@
+package com.pecbr.abateapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbateApiApplication.class, args);
+	}
+
+}
